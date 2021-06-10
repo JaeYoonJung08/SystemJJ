89c1c3313436342e351c25bdd64209a3e428d0db
jihyun-0611 <hello.git.ahi0611@gmail.com>
2021-06-10 13:58:17 +0900

    의류 목록을 객체에 저장

---
+++ System.js
@@ -0,0 +1,32 @@
+
+var row1 = {
+    number: 'YMP20103',
+    color: 'White',
+    siz: 'XS',
+    quantity: '3'
+};
+
+var row2 = {
+    number: 'YMP20208',
+    color: 'Black',
+    siz: 'M',
+    quantity: '9'
+};
+var row3 = {
+    number: 'YMP20321',
+    color: 'Black',
+    siz: 'L',
+    quantity: '3'
+};
+var row4 = {
+    number: 'YMP20114',
+    color: 'Grey',
+    siz: 'XL',
+    quantity: '5'
+};
+var row5 = {
+    number: 'YMP20101',
+    color: 'Blue',
+    siz: 'S',
+    quantity: '1'
+};
